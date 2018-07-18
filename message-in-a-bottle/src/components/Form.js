52a1a3d615c66794879b2d03edd795e15228d077
JohnZBurnett <tuninator@gmail.com>
2018-07-18 14:23:46 -0400

    control form

--- message-in-a-bottle/src/components/Form.js
+++ message-in-a-bottle/src/components/Form.js
@@ -3,6 +3,15 @@ import endpoints from '../endpoints.js'
 
 class Form extends Component {
 
+	constructor() {
+		super(); 
+
+		this.state = {
+			value: '',
+			selectValue: '',
+		};
+	}
+
 
   generateEndpointOptions = () => (
   	 endpoints.map((endpoint) => (
@@ -11,13 +20,27 @@ class Form extends Component {
   	)
   )
 
+  handleChange = (event) => {
+  	if (event.target.id === "endpoints") {
+  		this.setState({
+  			selectValue: event.target.value,
+  		})
+  	} else if (event.target.id === "name") {
+	  	this.setState({
+	  		value: event.target.value,
+	  	}) 	
+  	}
+  }
+
+
   render(){
   	return(
   	  <div>
   	  	<form>
-  	  	  <select id="endpoints">
-  	  	  	{this.generateEndpointOptions()}
+  	  	  <select id="endpoints" value={this.state.selectValue} onChange={(event) => this.handleChange(event)}>
+  	  	  	{this.generateEndpointOptions()}]
   	  	  </select>
+  	  	  <input type="text" id="name" value={this.state.value} onChange={(event) => this.handleChange(event)}/>
   	  	</form>
   	    {console.log("Form")}
   	  </div>
